a895bda6bb07d4b86bcbe2f1b1cd7166226a5e8f
foobar <petru.cervac@gmail.com>
2022-12-29 20:55:49 +0000

    feat: add a new target

--- build/Build.cs
+++ build/Build.cs
@@ -1,5 +1,7 @@
 using Nuke.Common;
 using Nuke.Common.Tooling;
+using static Nuke.Common.Tools.Npm.NpmTasks;
+using Nuke.Common.Tools.Npm;
 
 class Build : NukeBuild
 {
@@ -9,8 +11,13 @@ class Build : NukeBuild
 
 	[Parameter("Template to build")] readonly string Template;
 
+	Target InstallDevContainersCli => _ => _
+		.Executes(() => NpmInstall(_ => _
+			.SetPackages("@devcontainers/cli")
+			.SetGlobal(true)));
+
 	Target Compile => _ => _
 		.Requires(() => Template)
+		.DependsOn(InstallDevContainersCli)
 		.Executes(() => Bash($"./scripts/build.sh {Template}"));
-
 }
