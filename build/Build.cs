3ef059d8f16b637b1047871d7c11fd3fe37983dc
foobar <petru.cervac@gmail.com>
2022-12-29 18:14:40 +0000

    chore: clean up build project
    Remove comments and Configuration.cs file

--- build/Build.cs
+++ build/Build.cs
@@ -13,32 +13,22 @@ using static Nuke.Common.IO.PathConstruction;
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
+	public static int Main() => Execute<Build>(x => x.Compile);
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+	Target Clean => _ => _
+		.Before(Restore)
+		.Executes(() =>
+		{
+		});
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+	Target Restore => _ => _
+		.Executes(() =>
+		{
+		});
 
-    Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-        {
-        });
-
-    Target Restore => _ => _
-        .Executes(() =>
-        {
-        });
-
-    Target Compile => _ => _
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
-        });
-
+	Target Compile => _ => _
+		.DependsOn(Restore)
+		.Executes(() =>
+		{
+		});
 }
