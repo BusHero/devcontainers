70c44c6ed2fddcf6430b48ef61ec170665e49614
foobar <petru.cervac@gmail.com>
2022-12-30 09:01:48 +0000

    chore: update BuildTemplate target

--- build/Build.cs
+++ build/Build.cs
@@ -1,4 +1,5 @@
 using Nuke.Common;
+using Nuke.Common.IO;
 using Nuke.Common.Tooling;
 
 sealed class Build : NukeBuild
@@ -9,7 +10,9 @@ sealed class Build : NukeBuild
 
 	[Parameter("Template to build")] readonly string Template;
 
+	private AbsolutePath Scripts => RootDirectory / "scripts";
+
 	Target BuildTemplate => _ => _
 		.Requires(() => Template)
-		.Executes(() => Bash($"./scripts/build.sh {Template}"));
+		.Executes(() => Bash($"{Scripts / "build.sh"} {Template}"));
 }
