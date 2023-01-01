6739976cfc33f8f00852a9763e267b423605bbf5
foobar <petru.cervac@gmail.com>
2023-01-01 19:13:07 +0000

    chore: add Build.Templates.cs

---
+++ build/Build.Templates.cs
@@ -0,0 +1,26 @@
+using Nuke.Common;
+using Nuke.Common.IO;
+
+sealed partial class Build
+{
+	private AbsolutePath Scripts => RootDirectory / "scripts";
+	private AbsolutePath Source => RootDirectory / "src";
+
+	[Parameter("Template to build")] private readonly string Template;
+
+	private Target BuildTemplate => _ => _
+		.Requires(() => Template)
+		.Triggers(TestTemplate)
+		.Executes(() => Bash($"{Scripts / "build.sh"} {Template}"));
+
+	private Target TestTemplate => _ => _
+		.Requires(() => Template)
+		.Executes(() => Bash($"{Scripts / "test.sh"} {Template}"));
+
+	private Target PublishTemplate => _ => _
+		.DependsOn(InstallDevcontainer)
+		.Executes(() =>
+		{
+			return Devcontainer($"templates publish {Source} --namespace {GitHubNamespace}/templates");
+		});
+}
--- build/Build.cs
+++ build/Build.cs
@@ -1,5 +1,4 @@
 using Nuke.Common;
-using Nuke.Common.IO;
 using Nuke.Common.Tooling;
 using static Nuke.Common.Tools.Npm.NpmTasks;
 using Nuke.Common.Tools.Npm;
@@ -11,30 +10,10 @@ sealed partial class Build : NukeBuild
 	[PathExecutable("bash")] private readonly Tool Bash;
 	private Tool Devcontainer => ToolResolver.GetPathTool("devcontainer");
 
-	[Parameter("Template to build")] private readonly string Template;
-
-	private AbsolutePath Scripts => RootDirectory / "scripts";
-	private AbsolutePath Source => RootDirectory / "src";
-
-	private Target BuildTemplate => _ => _
-		.Requires(() => Template)
-		.Triggers(TestTemplate)
-		.Executes(() => Bash($"{Scripts / "build.sh"} {Template}"));
-
-	private Target TestTemplate => _ => _
-		.Requires(() => Template)
-		.Executes(() => Bash($"{Scripts / "test.sh"} {Template}"));
-
 	private Target InstallDevcontainer => _ => _
 		.Unlisted()
 		.Executes(() => NpmInstall(_ => _
 			.AddPackages("@devcontainers/cli")
 			.SetGlobal(true)));
 
-	private Target PublishTemplate => _ => _
-		.DependsOn(InstallDevcontainer)
-		.Executes(() =>
-		{
-			return Devcontainer($"templates publish {Source} --namespace {GitHubNamespace}/templates");
-		});
 }
