7883524da86efa92d73ddb21d3f2c928521dd85e
Cervac Petru <petru.cervac@gmail.com>
2023-01-01 17:02:19 +0200

    feat: install texlive (#16)
    * feat: install texlive
    
    * feat: add tests

--- features/src/texlive/install.sh
+++ features/src/texlive/install.sh
@@ -3,4 +3,20 @@
 apk update \
 	&& apk upgrade \
 	&& apk add \
+	curl \
+	wget \
+	perl-dev \
+	alpine-sdk \
+	texlive \
+	biber \
 	bash
+
+curl -L http://cpanmin.us | perl - App::cpanminus \
+	&& cpanm Log::Dispatch::File \
+	&& cpanm File::HomeDir \
+	&& cpanm Unicode::GCString \
+	&& cpanm YAML::Tiny
+
+wget https://mirrors.ctan.org/macros/latex/contrib/logreq.zip \
+	&& unzip logreq.zip -d /usr/share/texmf-dist/tex/latex/contrib \
+	&& mktexlsr
