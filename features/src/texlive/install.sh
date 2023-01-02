50c5dd626fd459c2a95a72958b6b9cb00fb1b2b3
Cervac Petru <petru.cervac@gmail.com>
2023-01-02 15:55:04 +0200

    feat: remove cpanm from texlive (#25)
    * chore: update test-pr.yaml
    
    * chore: remove cpanm from texlive

--- features/src/texlive/install.sh
+++ features/src/texlive/install.sh
@@ -3,19 +3,15 @@
 apk update \
 	&& apk upgrade \
 	&& apk add \
-	curl \
-	wget \
-	perl-dev \
 	alpine-sdk \
 	texlive \
 	biber \
 	bash
 
-curl -L http://cpanmin.us | perl - App::cpanminus \
-	&& cpanm Log::Dispatch::File \
-	&& cpanm File::HomeDir \
-	&& cpanm Unicode::GCString \
-	&& cpanm YAML::Tiny
+cpanm Log::Dispatch::File
+cpanm File::HomeDir
+cpanm Unicode::GCString
+cpanm YAML::Tiny
 
 wget https://mirrors.ctan.org/macros/latex/contrib/logreq.zip \
 	&& unzip logreq.zip -d /usr/share/texmf-dist/tex/latex/contrib \
