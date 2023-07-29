e1f8ca0de89d9afd4e806bc2ea62d0bd252dabe1
Cervac Petru <petru.cervac@gmail.com>
2023-07-29 18:16:32 +0300

    ci: add a new workflow (#38)
    * add a new workflow
    
    * ci: update continous.yaml
    
    * ci: update script\
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * asd
    
    * ci: setup node
    
    * asd
    
    * asd
    
    * asd
    
    * asd

--- build/Build.Templates.cs
+++ build/Build.Templates.cs
@@ -1,12 +1,44 @@
+using System.Text.Json;
 using Nuke.Common;
 using Nuke.Common.IO;
+using Serilog;
 
 sealed partial class Build
 {
 	private AbsolutePath Scripts => RootDirectory / "scripts";
 	private AbsolutePath Source => RootDirectory / "templates" / "src";
 
-	[Parameter("Template to build")] private readonly string Template;
+	[Parameter("Github Output")] private readonly string GithubOutput = null!;
+
+	[Parameter("Template to build")] private readonly string Template = null!;
+
+	private List<string?> Templates => Directory
+		.GetDirectories(Source)
+		.Select(Path.GetFileName)
+		.ToList();
+
+	private List<string?> Features => Directory
+		.GetDirectories(RootDirectory / "features" / "src")
+		.Select(Path.GetFileName)
+		.ToList();
+
+	private Target ListTemplates => _ => _
+	 	.Requires(() => GithubOutput)
+		.Executes(() =>
+		{
+			var templates = JsonSerializer.Serialize(Templates);
+			Log.Information("templates={Templates}", templates);
+			var features = JsonSerializer.Serialize(Features);
+			Log.Information("features={Features}", features);
+
+			File.WriteAllLines(
+				GithubOutput,
+				new[]
+				{
+					$"templates={templates}",
+					$"features={features}"
+				});
+		});
 
 	private Target BuildTemplate => _ => _
 		.Requires(() => Template)
@@ -18,7 +50,6 @@ sealed partial class Build
 		.Executes(() => Bash($"{Scripts / "test.sh"} {Template}"));
 
 	private Target PublishTemplate => _ => _
-		.DependsOn(InstallDevcontainer)
 		.Executes(() =>
 		{
 			return Devcontainer($"templates publish {Source} --namespace {GitHubNamespace}/templates");
--- build/Build.cs
+++ build/Build.cs
@@ -1,9 +1,8 @@
 using Nuke.Common;
 using Nuke.Common.Tooling;
-using static Nuke.Common.Tools.Npm.NpmTasks;
-using Nuke.Common.Tools.Npm;
 using Nuke.Common.Git;
 using Nuke.Common.Tools.GitHub;
+using Serilog;
 
 sealed partial class Build : NukeBuild
 {
@@ -12,13 +11,12 @@ sealed partial class Build : NukeBuild
 	[PathExecutable("bash")] private readonly Tool Bash;
 	private Tool Devcontainer => ToolResolver.GetPathTool("devcontainer");
 
-	private Target InstallDevcontainer => _ => _
-		.Unlisted()
-		.Executes(() => NpmInstall(_ => _
-			.AddPackages("@devcontainers/cli")
-			.SetGlobal(true)));
-
 	[GitRepository] private readonly GitRepository Repository;
 
 	private string GitHubNamespace => $"{Repository.GetGitHubOwner()}/{Repository.GetGitHubName()}";
+
+	private void DevcontainerLog(OutputType type, string message)
+	{
+		Log.Debug("{Message}", message);
+	}
 }
