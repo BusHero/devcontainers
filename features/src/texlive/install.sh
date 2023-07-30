3089d037e6d24487fbf9763329de3eec8a7d908d
foobar <petru.cervac@gmail.com>
2023-07-30 06:10:34 +0000

    feat: add paramter to texlive

--- features/src/texlive/install.sh
+++ features/src/texlive/install.sh
@@ -29,11 +29,13 @@ mkdir -p /root
 cp common/* /root/
 cd /root
 
-echo "binary_x86_64-linuxmusl 1" >> /root/texlive.profile \
-  && /root/install-texlive.sh $texlive_version \
-  && sed -e 's/ *#.*$//' -e '/^ *$/d' /root/packages.txt | \
-  xargs tlmgr install \
-  && rm -f /root/texlive.profile \
+echo "binary_x86_64-linuxmusl 1" >> /root/texlive.profile
+/root/install-texlive.sh $texlive_version
+tlmgr install listings
+if [ $PACKAGES ]; then
+  sed -e 's/,/ /g' $PACKAGES | xargs tlmgr install
+fi
+rm -f /root/texlive.profile \
   /root/install-texlive.sh \
-  /root/packages.txt \
-  && TERM=dumb luaotfload-tool --update
+  /root/packages.txt
+TERM=dumb luaotfload-tool --update
