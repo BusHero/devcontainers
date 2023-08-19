5189e1949bff64e7ab61306e8fbbd5d313a266c1
Cervac Petru <petru.cervac@gmail.com>
2023-08-19 17:34:33 +0100

    fix: fix nuke (#44)
    * feat: fix broken nuke for real
    
    * chore: remove unnecessary file

--- features/src/nuke/install.sh
+++ features/src/nuke/install.sh
@@ -1,12 +1,39 @@
 #!/bin/sh
 
-if command -v apt; then
-	sh install-dotnet-debian.sh
-elif command -v apk; then
-	sh install-dotnet-alpine.sh
+
+if ! command -v dotnet; then
+	if command -v apt; then
+		sh install-dotnet-debian.sh
+	elif command -v apk; then
+		sh install-dotnet-alpine.sh
+	fi
+fi
+
+if ! command -v bash; then
+	apk add bash
+fi
+
+if command -v nuke; then
+	exit 0
 fi
 
 dotnet tool \
 	install \
 	Nuke.GlobalTool \
 	--tool-path $_REMOTE_USER_HOME/.dotnet/tools
+
+echo '
+_nuke_bash_complete()
+{
+    local word=${COMP_WORDS[COMP_CWORD]}
+    local completions="$(nuke :complete "${COMP_LINE}")"
+    COMPREPLY=( $(compgen -W "$completions" -- "$word") )
+}
+complete -f -F _nuke_bash_complete nuke
+' >> $_REMOTE_USER_HOME/.bashrc
+
+
+if command -v chown; then
+	chown -R "$_REMOTE_USER:$_REMOTE_USER" "$_REMOTE_USER_HOME/.dotnet"
+	chown -R "$_REMOTE_USER:$_REMOTE_USER" "$_REMOTE_USER_HOME/.bashrc"
+fi
