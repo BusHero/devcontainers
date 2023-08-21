6b142cd167fbd68d424475cb66f61585ee3c7fbb
foobar <petru.cervac@gmail.com>
2023-08-21 18:19:20 +0000

    fix: fix bug with installing packages

---
+++ features/test/texlive/two-packages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "packages: $packages"
+
+echo "\documentclass[12pt,a4paper]{article} \usepackage{listings} \usepackage{subfiles} \begin{document}Hi\end{document}" > "test.tex"
+
+pdflatex -interaction=nonstopmode test.tex
