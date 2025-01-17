ae626cfa8933a9cd3b709417aee9f52294eb2460
Petru Cervac <petru.cervac@gmail.com>
2025-01-17 21:23:13 +0000

    feat: update configuration files

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,12 +4,12 @@ set -e
 
 echo "Skip"
 
-# SRC_DIR="/tmp/${TEMPLATE_ID}"
+SRC_DIR="/tmp/${TEMPLATE_ID}"
 # echo "Running Smoke Test"
 
-# ID_LABEL="test-container=${TEMPLATE_ID}"
+ID_LABEL="test-container=${TEMPLATE_ID}"
 # devcontainer exec --workspace-folder "${SRC_DIR}" --id-label ${ID_LABEL} bash -c 'set -e && if [ -f "test-project/test.sh" ]; then cd test-project && bash ./test.sh; else ls -a; fi' 2>&1
 
 # # Clean up
-# docker rm -f $(docker container ls -f "label=${ID_LABEL}" -q)
-# rm -rf "${SRC_DIR}"
+docker rm -f $(docker container ls -f "label=${ID_LABEL}" -q)
+rm -rf "${SRC_DIR}"
