f9a0961f4c22c8353faa320cfae0b1d312d20911
Petru Cervac <petru.cervac@gmail.com>
2025-01-17 19:54:07 +0000

    test: skip running tests for templates

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,12 +2,14 @@
 TEMPLATE_ID="$1"
 set -e
 
-SRC_DIR="/tmp/${TEMPLATE_ID}"
-echo "Running Smoke Test"
+echo "Skip"
 
-ID_LABEL="test-container=${TEMPLATE_ID}"
-devcontainer exec --workspace-folder "${SRC_DIR}" --id-label ${ID_LABEL} bash -c 'set -e && if [ -f "test-project/test.sh" ]; then cd test-project && bash ./test.sh; else ls -a; fi' 2>&1
+# SRC_DIR="/tmp/${TEMPLATE_ID}"
+# echo "Running Smoke Test"
 
-# Clean up
-docker rm -f $(docker container ls -f "label=${ID_LABEL}" -q)
-rm -rf "${SRC_DIR}"
+# ID_LABEL="test-container=${TEMPLATE_ID}"
+# devcontainer exec --workspace-folder "${SRC_DIR}" --id-label ${ID_LABEL} bash -c 'set -e && if [ -f "test-project/test.sh" ]; then cd test-project && bash ./test.sh; else ls -a; fi' 2>&1
+
+# # Clean up
+# docker rm -f $(docker container ls -f "label=${ID_LABEL}" -q)
+# rm -rf "${SRC_DIR}"
