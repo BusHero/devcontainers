230eb2f2540ce42dbef1bcff72ceef43cea2b365
Petru Cervac <petru.cervac@gmail.com>
2025-01-17 21:26:32 +0000

    chore: fix test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,5 +11,5 @@ ID_LABEL="test-container=${TEMPLATE_ID}"
 # devcontainer exec --workspace-folder "${SRC_DIR}" --id-label ${ID_LABEL} bash -c 'set -e && if [ -f "test-project/test.sh" ]; then cd test-project && bash ./test.sh; else ls -a; fi' 2>&1
 
 # # Clean up
-docker rm -f $(docker container ls -f "label=${ID_LABEL}" -q)
-rm -rf "${SRC_DIR}"
+# docker rm -f $(docker container ls -f "label=${ID_LABEL}" -q)
+# rm -rf "${SRC_DIR}"
